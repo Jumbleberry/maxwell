707b3e16da1c66e3238088f68fb5228472637958
Hao <hao.ling@jumbleberry.com>
2016-06-06 15:29:10 -0400

    Add queue and in flight

--- src/main/java/com/zendesk/maxwell/producer/KinesisProducer.java
+++ src/main/java/com/zendesk/maxwell/producer/KinesisProducer.java
@@ -1,8 +1,8 @@
 package com.zendesk.maxwell.producer;
 
-import java.io.File;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -24,8 +24,9 @@ public class KinesisProducer extends AbstractProducer {
     static final Logger LOGGER = LoggerFactory.getLogger(KinesisProducer.class);
     static final AtomicLong counter = new AtomicLong();
     private final com.amazonaws.services.kinesis.producer.KinesisProducer kinesis;
-    private final HashMap<String, LinkedBlockingQueue<RowMap>> messageQueue;
-    private int messageQueueSize;
+    private final HashMap<String, LinkedBlockingQueue<RowMap>> queue;
+    private final ConcurrentHashMap<String, RowMap> inFlight;
+    private int queueSize;
     private String streamName;
 
     public KinesisProducer(
@@ -55,8 +56,10 @@ public class KinesisProducer extends AbstractProducer {
         this.kinesis = new com.amazonaws.services.kinesis.producer.KinesisProducer(config);
         
         // Set up message queue
-        this.messageQueue = new HashMap<String, LinkedBlockingQueue<RowMap>>();
-        this.messageQueueSize = 0;
+        this.queue = new HashMap<String, LinkedBlockingQueue<RowMap>>();
+        this.queueSize = 0;
+        this.inFlight = new ConcurrentHashMap<String, RowMap>();
+
         this.streamName = kinesisStreamName;
     }
 
@@ -66,51 +69,81 @@ public class KinesisProducer extends AbstractProducer {
         String key = r.getTable() + r.pkAsConcatString();
 
         // Initialize list if none exist
-        if (! messageQueue.containsKey(key)) {
-        	messageQueue.put(key, new LinkedBlockingQueue<RowMap>());
+        // and add to in flight queue
+        if (! queue.containsKey(key)) {
+            queue.put(key, new LinkedBlockingQueue<RowMap>());
+            addToInFlight(key, r);
         }
         
-    	// Add to it's own list in the message queue
-        LinkedBlockingQueue<RowMap> list = messageQueue.get(key);
-        list.add(r);
-        ++this.messageQueueSize;
+        addToQueue(key, r);
+    }
 
-        // If this is the only element in list
-        if (list.size() == 1) {
-            pushToKinesis(key, r);
-        }
+    protected void addToQueue(String key, RowMap r) throws Exception {
+        queue.get(key).add(r);
+        ++queueSize;
+    }
+
+    protected void removeFromQueue(String key, RowMap r) throws Exception {
+        queue.get(key).remove(r);
+        --queueSize;
+    }
+
+    protected RowMap getNextInQueue(String key, RowMap r) throws InterruptedException {
+        LinkedBlockingQueue<RowMap> list = queue.get(key);
+        list.take();
+        --queueSize;
+        return list.peek();
+    }
+
+    protected void addToInFlight(String key, RowMap r) throws Exception {
+        inFlight.put(key, r);
+        pushToKinesis(key, r);
     }
     
-    /**
-     * Push data to Kinesis
-     * 
-     * @param  String   partitionKey 
-     * @param  RowMap   r
-     * 
-     * @throws Exception    
-     */
-    private void pushToKinesis(String partitionKey, RowMap r) throws Exception {
+    protected void removeFromInFlight(String key, RowMap r) throws Exception {
+        inFlight.remove(key);
+        context.setPosition(r);
+    }
+
+    protected void pushToKinesis(String key, RowMap r) throws Exception {
     	
         ByteBuffer data = ByteBuffer.wrap(r.toAvro().toByteArray());
 
         FutureCallback<UserRecordResult> callBack = 
-            new FutureCallback<UserRecordResult>() {
+            (new FutureCallback<UserRecordResult>() {
+                protected String key;
+        		protected RowMap r;
+        	
                 @Override public void onFailure(Throwable t) {
-    	           // Upon failure, get minimum position of binlog and re-try
+                	// Upon failure, get minimum position of binlog and re-try
+                	// TODO
                     System.out.println("Failed:" + t.toString()); 
-                };     
-                @Override 
-                public void onSuccess(UserRecordResult result) { 
-                   // Upon success, this.context.setPosition(r);
-                    System.out.println("Success: " + result.toString());
-                    // messageQueue.get(partitionKey).remove(r);
                 };
-            };
+
+                @Override 
+                public void onSuccess(UserRecordResult result) {
+                    System.out.println("Success: " + result.toString());
+                    try {
+                    	removeFromInFlight(key, r);
+                        RowMap next = getNextInQueue(key, r);
+                        if (next != null) {
+                    	   addToInFlight(key, next);
+                        }
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+                };
+                
+                public FutureCallback<UserRecordResult> setUp(String key, RowMap r) {
+                    this.key = key;
+                	this.r = r;
+                	return this;
+                }
+            }).setUp(key, r);
 
         ListenableFuture<UserRecordResult> response =
-                this.kinesis.addUserRecord(streamName, partitionKey, data);
-        System.out.println("Writing data...");
+                this.kinesis.addUserRecord(streamName, key, data);
 
-        Futures.addCallback(response, callBack);        
+        Futures.addCallback(response, callBack);
     }
 }
