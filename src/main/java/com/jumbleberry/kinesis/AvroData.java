72b622cf218936984c3338e866c32be8a8751854
obiwongee <dave.wong@jumbleberry.com>
2016-06-21 10:57:11 -0400

    changed primary_key to column names remove unused imports

--- src/main/java/com/jumbleberry/kinesis/AvroData.java
+++ src/main/java/com/jumbleberry/kinesis/AvroData.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.avro.Schema;
